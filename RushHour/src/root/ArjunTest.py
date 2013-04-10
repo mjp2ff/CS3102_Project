f30e594b86c880ab07819d9316c2cf6e2c1165f1
mjp2ff <mjp2ff@virginia.edu>
2013-04-09 23:41:50 -0400

    blah blah blah

--- RushHour/src/root/ArjunTest.py
+++ RushHour/src/root/ArjunTest.py
@@ -20,38 +20,26 @@ can.grid()
 # note that rect is actually just an integer that is used to identify 
 # that shape in the context of the canvas that it was created within. 
 rect = can.create_rectangle(400,0,600,200,fill=rectanglecolor) 
-
-x = 0
-y = 0
-def somethingClicked(event):
-    global x, y
-    x = event.x
-    y = event.y
-    print x, y
+            
+def somethingMoved(event):
     tuple = can.bbox(rect)
+    global x, y
     
     # array that this loop creates is x1, y1, x2, y2
     loc = {}
     for num, entry in enumerate(tuple):
         loc[num] = entry
-    if x < loc[0] or x > loc[2]:
-        x = 1000
-    if y < loc[1] or y > loc[3]:
-        y = 1000
-
-def doneClick(event):
-    if x == 1000 or y == 1000:
-        print x, y
-        return
-    else:
-        print event.x-x, event.y-y
-        can.move(rect, event.x-x, event.y-y)
-        root.update()
-        root.after(30)
+    x = (loc[0]+loc[2])/2
+    y = (loc[1]+loc[3])/2
+    if event.x < loc[0] or event.x > loc[2] or event.y < loc[1] or event.y > loc[3]:
+        return False
+    
+    can.move(rect, event.x-x, event.y-y)
+    root.update()
+    root.after(30)
 
 frame = Frame(root, width=800, height=450)
-can.bind("<Button-1>", somethingClicked)
-can.bind("<ButtonRelease-1>", doneClick)
+can.bind("<B1-Motion>", somethingMoved)
 
 # sample code to move a rectangle
 # for i in range(100): 
