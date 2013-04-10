8d353f76bbaea47fa714e10c45aec68e531232c2
Arjun <sarjun93@gmail.com>
2013-04-09 23:53:04 -0400

    Can drag the rectangle now

--- RushHour/src/root/ArjunTest.py
+++ RushHour/src/root/ArjunTest.py
@@ -20,34 +20,32 @@ can.grid()
 # note that rect is actually just an integer that is used to identify 
 # that shape in the context of the canvas that it was created within. 
 rect = can.create_rectangle(400,0,600,200,fill=rectanglecolor) 
-            
-def somethingMoved(event):
-    tuple = can.bbox(rect)
-    global x, y
-    
-    # array that this loop creates is x1, y1, x2, y2
-    loc = {}
-    for num, entry in enumerate(tuple):
-        loc[num] = entry
-    x = (loc[0]+loc[2])/2
-    y = (loc[1]+loc[3])/2
-    if event.x < loc[0] or event.x > loc[2] or event.y < loc[1] or event.y > loc[3]:
-        return False
-    
-    can.move(rect, event.x-x, event.y-y)
-    root.update()
-    root.after(30)
+
+x = 0
+y = 0
+
+firstTime = True
+def dragMove(event):
+    global firstTime
+    if firstTime:
+        firstTime = False
+        global x, y
+        x = event.x
+        y = event.y
+    else:
+        can.move(rect, event.x-x, event.y-y)
+        root.update()
+        global x, y
+        x = event.x
+        y = event.y
+
+def resetFirstTime(event):
+    global firstTime
+    firstTime = True
 
 frame = Frame(root, width=800, height=450)
-can.bind("<B1-Motion>", somethingMoved)
-
-# sample code to move a rectangle
-# for i in range(100): 
-#     # move the rectangle 0 in the x direction and disty in the y direction. 
-#     can.move(rect, 0, distY) 
-#     root.update() # update the display 
-#     root.after(30) # wait 30 ms 
-
+can.bind("<B1-Motion>", dragMove);
+can.bind("<ButtonRelease-1>", resetFirstTime)
 
 # start the game
 root.mainloop()
